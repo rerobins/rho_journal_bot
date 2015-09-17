d4c8a3bc973752d7aec6ca20e4d150694034a54d
Robert Robinson <rerobins@gmail.com>
2015-09-17 12:53:04 -0600

    Save event that is created

--- journal_bot/components/commands/create_event.py
+++ journal_bot/components/commands/create_event.py
@@ -4,12 +4,9 @@ Command that will allow for the creation of arbitrary events in the database.
 from rhobot.components.rdf_publish import RDFSourceStanza
 from rhobot.components.commands.base_command import BaseCommand
 from rhobot.components.storage import StoragePayload
-from rhobot.namespace import GRAPH
+from rhobot.namespace import GRAPH, WGS_84, SCHEMA, EVENT, TIMELINE, RHO
+from rdflib.namespace import DCTERMS, DC, FOAF
 import logging
-from rdflib.namespace import Namespace
-
-SCHEMA = Namespace('http://schema.org/')
-WGS_84 = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
 
 logger = logging.getLogger(__name__)
 
@@ -18,13 +15,18 @@ class CreateEventCommand(BaseCommand):
     def initialize_command(self):
         super(CreateEventCommand, self).initialize_command()
         self._initialize_command(identifier='create_event', name='Create Event',
-                                 additional_dependencies={'rho_bot_rdf_publish', 'rho_bot_storage_client', })
+                                 additional_dependencies={'rho_bot_rdf_publish', 'rho_bot_storage_client',
+                                                          'rho_bot_get_or_lookup', 'rho_bot_scheduler',
+                                                          'rho_bot_representation_manager', })
 
     def post_init(self):
         super(CreateEventCommand, self).post_init()
 
+        self._scheduler = self.xmpp['rho_bot_scheduler']
         self._rdf_publish = self.xmpp['rho_bot_rdf_publish']
         self._storage_client = self.xmpp['rho_bot_storage_client']
+        self._get_or_lookup = self.xmpp['rho_bot_get_or_lookup']
+        self._representation_manager = self.xmpp['rho_bot_representation_manager']
 
     def command_start(self, request, initial_session):
         """
@@ -74,11 +76,134 @@ class CreateEventCommand(BaseCommand):
         return self._rdf_publish.send_out_search(payload, timeout=2.0).then(handle_results_from_search)
 
     def store_results(self, payload, session):
+        """
+        Store the results into the database.
+        :param payload:
+        :param session:
+        :return:
+        """
 
         logger.info('Payload: %s' % payload)
         logger.info('Session: %s' % session)
 
+        storage_session = dict()
+
+        if 'locations' in payload.get_values():
+            storage_payload = StoragePayload()
+            storage_payload.about = payload.get_values()['locations']
+            storage_payload.add_type(WGS_84.SpatialThing)
+
+            promise = self._get_or_lookup(storage_payload).then(
+                self._scheduler.generate_promise_handler(self._update_session, storage_session, 'location'))
+        else:
+            promise = self._scheduler.defer(lambda: storage_session)
+
+        promise = promise.then(self._get_owner)
+        promise = promise.then(self._scheduler.generate_promise_handler(self._create_interval, payload))
+        promise = promise.then(self._scheduler.generate_promise_handler(self._create_event, payload))
+
+        promise = promise.then(lambda s: session)
+
+        session['payload'] = None
+        session['next'] = None
+        session['has_next'] = False
+
+        return promise
+
+    def _update_session(self, result, session, key):
+        """
+        Update the storage session with the key and value.
+        :param result: value to store.
+        :param session: session to store values in.
+        :param key: key to store value under.
+        :return: session.
+        """
+        session[key] = result
+
         return session
+
+    def _get_owner(self, session):
+        """
+        Fetch the owner of the system from the framework.
+        :param session:
+        :return:
+        """
+        def _translate_search_to_about(result):
+            if result.results:
+                return result.results[0].about
+
+            raise RuntimeError('Owner could not be found')
+
+        search_payload = StoragePayload()
+        search_payload.add_type(RHO.Owner, FOAF.Person)
+
+        promise = self._rdf_publish.send_out_request(search_payload)
+        promise = promise.then(_translate_search_to_about)
+        promise = promise.then(self._scheduler.generate_promise_handler(self._update_session, session, 'owner'))
+
+        return promise
+
+    def _create_interval(self, session, form_payload):
+        """
+        Convert the form payload into a storage payload for creating a new interval.
+        :param session:
+        :param form_payload:
+        :return:
+        """
+        def _translate_search_to_about(result):
+            if result.results:
+                return result.results[0].about
+
+            raise RuntimeError('Interval was not created.')
+
+        # Handle the creation of the interval.
+        create_interval_payload = StoragePayload()
+        create_interval_payload.add_type(TIMELINE.Interval)
+
+        if 'event_start' in form_payload.get_values():
+            create_interval_payload.add_property(TIMELINE.start, form_payload.get_values()['event_start'])
+
+        if 'event_stop' in form_payload.get_values():
+            create_interval_payload.add_property(TIMELINE.end, form_payload.get_values()['event_stop'])
+
+        creator = self._representation_manager.representation_uri
+        if creator:
+            create_interval_payload.add_property(DCTERMS.creator, creator)
+
+        promise = self._storage_client.create_node(create_interval_payload)
+        promise = promise.then(_translate_search_to_about)
+        promise = promise.then(self._scheduler.generate_promise_handler(self._update_session, session, 'interval'))
+
+        return promise
+
+    def _create_event(self, session, form_payload):
+        """
+        Conver the form payload into a storage payload for creating a new event.
+        :param session:
+        :param form_payload:
+        :return:
+        """
+        create_event_payload = StoragePayload()
+        create_event_payload.add_type(EVENT.Event)
+
+        create_event_payload.add_reference(key=EVENT.agent, value=session['owner'])
+        create_event_payload.add_reference(key=DCTERMS.creator, value=self._representation_manager.representation_uri)
+
+        if 'title' in form_payload.get_values():
+            create_event_payload.add_property(key=DC.title, value=form_payload.get_values()['title'])
+
+        if 'description' in form_payload.get_values():
+            create_event_payload.add_property(key=DC.description, value=form_payload.get_values()['description'])
+
+        if session['location']:
+            create_event_payload.add_reference(key=EVENT.place, value=session['location'])
+
+        if session['interval']:
+            create_event_payload.add_reference(key=EVENT.time, value=session['interval'])
+
+        promise = self._storage_client.create_node(create_event_payload)
+
+        return promise
 
 
 create_event = CreateEventCommand
